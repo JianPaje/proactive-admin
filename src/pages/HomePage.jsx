5fdf76a13b9d408d0317da3d2421262f3bd7f3ba
Vince Jhonas O. Rodriguez <jhonasvincerodriguez@gmail.com>
2025-07-21 16:51:40 +0800

    fix: the name

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../assets/logo.png';
-import appScreenshot from '../assets/RetroPic.jpeg'; 
+import appScreenshot from '../assets/retroPic.jpeg'; 
 
 const HomePage = () => {
   return (
