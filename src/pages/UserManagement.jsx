37a2683dc5f312499e734858201bbd9c0b4225bb
Vince Jhonas O. Rodriguez <jhonasvincerodriguez@gmail.com>
2025-07-28 21:08:12 +0800

    resend fix

--- src/pages/UserManagement.jsx
+++ src/pages/UserManagement.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../supabaseClient.js';
 import UserDetailModal from '../components/UserDetailModal.jsx';
 
 
